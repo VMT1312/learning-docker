cfe9ca30a1da232babbc146bd7bbd5f27d43178a
VMT1312 <thiemmavinh123@gmail.com>
2025-08-16 10:40:40 +0800

    Update Dockerfile to use python3 and ensure proper package installation

--- Dockerfile.py
+++ Dockerfile.py
@@ -1,4 +1,12 @@
+# Build from a slim Debian/Linux image
 FROM debian:stable-slim
+
+# Update apt and upgrade packages
+RUN apt update && apt upgrade -y
+
+# Install Python 3
+RUN apt install -y python3
+
 COPY main.py main.py
 COPY books/ books/
-CMD ["python", "main.py"]
+CMD ["python3", "main.py"]
